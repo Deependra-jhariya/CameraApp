6b1559d4a17fe740431dbc6ddffab2048110a5d7
Deependra-jhariya <deependrajhariya@gmail.com>
2025-09-15 16:18:31 +0530

    setup project

---
+++ src/navigation/AllStack/AllStack.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AllStack = () => {
+  return (
+    <View>
+      <Text>AllStack</Text>
+    </View>
+  )
+}
+
+export default AllStack
+
+const styles = StyleSheet.create({})
